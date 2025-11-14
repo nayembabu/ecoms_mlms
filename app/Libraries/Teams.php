f041b1315939a142e5549b4ecdb17bcfd6d6ebbd
ABUNAYEM <babunayem@gmail.com>
2025-11-14 11:29:51 +0600

    product page added

--- app/Libraries/Teams.php
+++ app/Libraries/Teams.php
@@ -18,6 +18,7 @@ class Teams
 
     public function enroll_product_self($product_id, $user_id, $buying_id)
     {
+        $userInfoId = $this->session->get('userInfoId');
         $user_buying_id = $this->db->table('product_information')
                                     ->where('id', $buying_id)
                                     ->get()
@@ -46,12 +47,30 @@ class Teams
                                     ->getRow();
         if ($current_wallet_balance >= $user_buying_id->buying_price) {
             // Deduct wallet amount
+            if ($temp_user_reffer) {
+                $this->db->table('user_reffer')->insert([
+                    'reffer_ref_user_idd'   => $temp_user_reffer->ref_reffer_user_idd,
+                    'reffer_main_idd'       => $temp_user_reffer->rreffer_main_id,
+                    'timessstamps'          => time(),
+                ]);
+                $this->db->table('temp_user_reffer')
+                         ->where('ref_reffer_user_idd', $userInfoId)
+                         ->delete();
+                $this->team_check_and_increement($userInfoId);
+            }else {
+                //
+            }
         } else {
             // Insufficient balance
             $data['status'] = 'error';
             $data['message'] = 'Insufficient wallet balance.';
             return json_encode($data)->with(200);
         }
+    }
+
+    public function team_check_and_increement($userInfoId)
+    {
+
     }
 
 
